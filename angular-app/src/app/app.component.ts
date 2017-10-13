32e6704fb697e69f58fb1198a96ded1f7c9e6eca
yong-fu <15026929959@139.com>
2017-10-13 17:24:33 +0800

    Uploaded project files

---
+++ angular-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component }          from '@angular/core';
+
+@Component({
+  selector: 'alva-angular-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = '爱华（AIVA）人工智能数据架构战略咨询工作室';
+}
